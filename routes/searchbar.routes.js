30b56ceb26197d9bd72ce792b3a0fd31fbbe26fc
olivermnoo <riseumpro@gmail.com>
2023-03-08 01:41:34 -0300

    searchbar done

---
+++ routes/searchbar.routes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { TabModel } from "../models/tab.model.js";
+
+const searchbarRouter = express.Router();
+
+searchbarRouter.get("/:term", async (req, res) => {
+  try {
+    const term = req.params.term;
+
+    const tabs = await TabModel.find({
+      $or: [
+        { title: { $regex: term, $options: "i" } },
+        { category: { $regex: term, $options: "i" } },
+      ],
+    });
+
+    res.status(200).json(tabs);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json(error);
+  }
+});
+
+export { searchbarRouter };
